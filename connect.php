8bfc7a5e0aedf53bd74f4f9f1828d51892750008
Thunderer27121 <anujsingh27121@gmail.com>
2025-08-20 16:29:43 +0530

    last update

--- connect.php
+++ connect.php
@@ -1,5 +1,10 @@
 <?php
-$con = mysqli_connect('localhost','root','','platform');
+$db_host = "mysql-66d3913-anujsingh27121-bc28.f.aivencloud.com";
+$port = 27976;
+$db_name = "defaultdb";
+$user = "avnadmin";
+$pass = "AVNS_g6Lw4jYfacT3mZPvA2L";
+$con = mysqli_connect($db_host,$user,$pass,$db_name,$port);
 if(!$con){
     die('unable to connect').mysqli_error($con);
 }
