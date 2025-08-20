dedebf3ec1cf0c18fdc1a321ade536cb2b18dab6
Anuj <anujsingh27121@gmail.com>
2025-08-20 17:41:59 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
-$host   = $_ENV['db_host'];
-$port   = (int)$_ENV['port'];
-$user   = $_ENV['user'];
-$pass   = $_ENV['pass'];
-$dbname = $_ENV['db_name'];
+$host = getenv("db_host");
+$port = getenv("port");
+$user = getenv("user");
+$pass = getenv("pass");
+$db_name = getenv("db_name");
 
-$con = mysqli_connect($db_host,$user,$pass,$db_name,$port);
+$con = mysqli_connect($host,$user,$pass,$db_name,$port);
 if(!$con){
     die('unable to connect').mysqli_error($con);
 }
