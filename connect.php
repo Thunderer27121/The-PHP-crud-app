6a526dabbc42dcd0db66ff79d90dab22e6629b78
Anuj <anujsingh27121@gmail.com>
2025-08-20 17:37:38 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,4 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 $host   = $_ENV['db_host'];
 $port   = (int)$_ENV['port'];
 $user   = $_ENV['user'];
