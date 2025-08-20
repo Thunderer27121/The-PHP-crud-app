846ba0c5154423eb12bb248816c408fd926335b0
Thunderer27121 <anujsingh27121@gmail.com>
2025-08-20 19:09:01 +0530

    change of data

--- registercode.php
+++ registercode.php
@@ -15,12 +15,12 @@ function sendemail($name, $email, $verify_token)
         //Server settings
         $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->Host       = getenv("SMTP_HOST");                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'anujsingh27121@gmail.com';                     //SMTP username
-        $mail->Password   = 'iujulevyrjulcgau';                               //SMTP password
+        $mail->Username   = getenv("SMTP_USER");                     //SMTP username
+        $mail->Password   = getenv("SMTP_PASS");              //SMTP password
         $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->Port       = getenv("SMTP_PORT");      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
         $mail->setFrom('anujsingh27121@gmail.com');
