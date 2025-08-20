761cbab789fd39af12e86c33e07efa01d7554800
Thunderer27121 <anujsingh27121@gmail.com>
2025-08-20 21:10:41 +0530

    try

--- registercode.php
+++ registercode.php
@@ -13,7 +13,6 @@ function sendemail($name, $email, $verify_token)
 
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = getenv("SMTP_HOST");                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
