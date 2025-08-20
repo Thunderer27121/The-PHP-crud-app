5d16ad667cda87481e2844728f2fa9175d224d95
Thunderer27121 <anujsingh27121@gmail.com>
2025-08-20 17:51:58 +0530

    apt.txt

--- connect.php
+++ connect.php
@@ -1,15 +1,13 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-$host   = $_ENV['db_host'];
-$port   = (int)$_ENV['port'];
-$user   = $_ENV['user'];
-$pass   = $_ENV['pass'];
-$dbname = $_ENV['db_name'];
+$host = getenv("db_host");
+$port = getenv("port");
+$user = getenv("user");
+$pass = getenv("pass");
+$db_name = getenv("db_name");
 
-$con = mysqli_connect($db_host,$user,$pass,$db_name,$port);
+
+$con = mysqli_connect($host,$user,$pass,$db_name,$port);
 if(!$con){
     die('unable to connect').mysqli_error($con);
 }
